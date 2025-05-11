15ac8d5265070fcd99cc38b18c961f042b75af47
GoharAliAssort <gohar.2016623@gmail.com>
2025-05-11 21:17:31 +0500

    initial commit; All simple CRUD apis in index.js; connected to MongoDB.

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/product.model')
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.get('/',(req, res) => {
+    res.send('Hello form Express Get API');
+});
+
+app.post('/api/products',async (req, res) =>{
+    try {
+       const product = await Product.create(req.body);
+       res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/products',async (req, res) =>{
+    try {
+       const products = await Product.find();
+       res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/products/:id',async (req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+
+        if(!product) return res.status(404).json({message: 'Product not found'});
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});        
+    }
+});
+
+app.put('/api/products/:id',async (req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if(!product) return res.status(404).json({message: 'Product not found'});
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);   
+
+    } catch (error) {
+        res.status(500).json({message: error.message});        
+    }
+});
+
+app.delete('/api/products/:id',async (req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product) return res.status(404).json({message: 'Product not found'});
+        
+        res.status(200).json({message: 'Product deleted successfully'});
+    } catch (error) {
+        res.status(500).json({message: error.message});        
+    }
+});
+
+
+mongoose.connect('mongodb+srv://gohar2016623:2groGpyD2HbqfFdL@cluster0.oqlebou.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    console.log('Connected to database!');
+    app.listen(3000, () =>{
+        console.log('App is runnning on port 3000');
+    });
+})
+.catch(() => {
+    console.log("Couldn't connect to database");
+});
