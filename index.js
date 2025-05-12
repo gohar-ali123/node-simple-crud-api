6d1eae727e0477640adcc6a612c32af7773bcab6
Gohar Ali <goharali.1998.mech@gmail.com>
2025-05-12 11:16:46 +0500

    routes and controllers added for better management of code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/product.model')
+const ProductRoutes = require('./routes/product.routes');
 
 const app = express();
 
@@ -8,68 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+//Routes
+app.use("/api/products", ProductRoutes);
+
+
 
 app.get('/',(req, res) => {
     res.send('Hello form Express Get API');
-});
-
-app.post('/api/products',async (req, res) =>{
-    try {
-       const product = await Product.create(req.body);
-       res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.get('/api/products',async (req, res) =>{
-    try {
-       const products = await Product.find();
-       res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.get('/api/products/:id',async (req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-
-        if(!product) return res.status(404).json({message: 'Product not found'});
-
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});        
-    }
-});
-
-app.put('/api/products/:id',async (req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-
-        if(!product) return res.status(404).json({message: 'Product not found'});
-
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);   
-
-    } catch (error) {
-        res.status(500).json({message: error.message});        
-    }
-});
-
-app.delete('/api/products/:id',async (req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-
-        if(!product) return res.status(404).json({message: 'Product not found'});
-        
-        res.status(200).json({message: 'Product deleted successfully'});
-    } catch (error) {
-        res.status(500).json({message: error.message});        
-    }
 });
 
 
